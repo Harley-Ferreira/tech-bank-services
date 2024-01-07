f90549d0d62ca2781ee6395310405a6f840f4918
Harley <harley201009@hotmail.com>
2024-01-07 10:08:11 -0300

    feat: Creating credit appraiser project

---
+++ credit-appraiser-service/src/test/java/io/github/techbank/creditappraiserservice/CreditAppraiserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.techbank.creditappraiserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAppraiserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
